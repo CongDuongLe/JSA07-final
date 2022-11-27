222a6a52932d11ee5dc6083bd0db726ecfd40c94
CDlee <80387658+CongDuongLe@users.noreply.github.com>
2022-11-27 09:19:45 +0700

    buoi1jsa07nh

---
+++ README.md
@@ -0,0 +1,2 @@
+# JSA07NH
+git add . : thêm tất cả các file có sự thay đổi để push lên github
---
+++ src/javascript/Buoi1.js
@@ -0,0 +1,6 @@
+// nhiệm vụ : viết logic cho button để khi bấm vào 1 lần sẽ bật lên hộp thoại prompt 
+//và in thông tin người dung nhập vào ra màn hình 
+// 5 phút để nhớ lại bài cũ
+
+// gợi ý : lấy ra button trong DOM, nhớ tên DOM-event và viết 1 function để gán cho DOM-event đó
+
